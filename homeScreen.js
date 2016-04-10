1d2dc47617a9c6e72873558018295bdbd1fa2467
Elysa Kohrs <elysakohrs@gmail.com>
2016-04-10 16:02:37 -0400

    more dealing with adding new goals, adjusting placement on home screen

--- homeScreen.js
+++ homeScreen.js
@@ -18,7 +18,7 @@
 function getHtmlForNewGoal(name, amount) {
     var html ='<div class="col-md-3"><div id="new-goal-1" class="panel panel-default goal"><div class="goal-non-hover"><h4 class="text-center">';
     html += name;
-    html += '</h4><img src="http://placehold.it/350X350" class="goal-icon center-block"><h4 class="text-center">$0.00 Saved!</h4></div><div class="hover-add"><button class="btn btn-default hover-btn-top">Open Goal</button><button class="btn btn-default hover-btn-bottom">Add $ to Savings</button></div></div></div>';
+    html += '</h4><img src="http://placehold.it/350X350" class="goal-icon center-block"><div class="progress"><div class="progress-bar" style="width: 60%;"></div></div><h4 class="text-center progress-amount">$6.00/$10.00</h4></div><div class="hover-add"><button class="btn btn-default hover-btn-top">Open Goal</button><button class="btn btn-default hover-btn-bottom">Add $ to Savings</button></div></div></div>';
     return html;
 }
 

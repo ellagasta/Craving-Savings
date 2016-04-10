e47fd106a9a8817d167740404ff91e917338412b
Elysa Kohrs <elysakohrs@gmail.com>
2016-04-10 14:59:32 -0400

    fix some stuff, add hover

--- homeScreen.js
+++ homeScreen.js
@@ -4,11 +4,34 @@
 	// 	 $(this).tab('show');
 	// });
 
-	$(function () {
-		$('#myTabs a:first').tab('show') // Select first tab
+	// $(function () {
+	// 	$('#myTabs a:first').tab('show'); // Select first tab
 
-	})
-		$('#myTabs a').click(function (e) {
-		  e.preventDefault()
-		  $(this).tab('show')
-		})
+
+	// 	$('#myTabs a').click(function (e) {
+	// 	  		  console.log("hi");
+	// 		e.preventDefault();
+	// 	  $(this).tab('show');
+	// 	});
+	// })
+
+
+
+$(function () {
+	$('#myTab a:first').tab('show');
+
+	$('#myTab a').click(function (e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$('.goal').hover(function(){
+		$('.hover-add').fadeTo("fast", 1);
+		$('.goal-non-hover').fadeTo("fast", .5);
+	}, function()
+	{ 
+	    $('.hover-add').fadeTo("fast", 0);
+	    $('.goal-non-hover').fadeTo("fast", 1);
+	});
+
+})

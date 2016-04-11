2d9ac337edb8d64048884740caea2a9237a01e1f
Elysa Kohrs <elysakohrs@gmail.com>
2016-04-11 15:08:18 -0400

    work more on hover, add clicking affordances to add new goal panel:

--- homeScreen.js
+++ homeScreen.js
@@ -1,8 +1,10 @@
 function getHtmlForNewGoal(name, amount) {
-    var html ='<div class="col-md-3"><div id="new-goal-1" class="panel panel-default goal"><div class="goal-non-hover"><h4 class="text-center">';
-    html += name;
-    html += '</h4><img src="http://placehold.it/350X350" class="goal-icon center-block"><div class="progress"><div class="progress-bar" style="width: 60%;"></div></div><h4 class="text-center progress-amount">$6.00/$10.00</h4></div><div class="hover-add"><button class="btn btn-default hover-btn-top">Open Goal</button><button class="btn btn-default hover-btn-bottom">Add $ to Savings</button></div></div></div>';
-    return html;
+	var html = '<div class="col-md-3"><div class="panel panel-default goal hoverable-panel"><div class="non-hover non-hover-div"><h4 class="text-center">';
+	html += name;
+	html += '</h4><img src="http://placehold.it/350X350" class="goal-icon center-block"><div class="progress"><div class="progress-bar" style="width: 60%;"></div></div><h4 class="text-center progress-amount">';
+	html += '$0.00/$' + amount;
+	html += '</h4></div><div class="hover-add hover-div"><button class="btn btn-default hover-btn-top">Open Goal</button><button class="btn btn-default hover-btn-bottom">Add $ to Savings</button></div></div></div>';
+	return html;
 }
 
 $(function () {
@@ -13,17 +15,17 @@ $(function () {
 		$(this).tab('show');
 	});
 
-	$('.goal').hover(function(){
-		$('.hover-add').fadeTo("fast", 1);
-		$('.goal-non-hover').fadeTo("fast", .5);
-	}, function()
-	{ 
-	    $('.hover-add').fadeTo("fast", 0);
-	    $('.goal-non-hover').fadeTo("fast", 1);
+	$('.hoverable-panel').hover(function() {
+		console.log("hovering!" + this);
+		$(this).find('.hover-div').fadeTo("fast", 1);
+		$(this).find('.non-hover-div').fadeTo("fast", .5);
+	}, function() { 
+	    $(this).find('.hover-div').fadeTo("fast", 0);
+	    $(this).find('.non-hover-div').fadeTo("fast", 1);
 	});
 
 	$('.new-goal').click(function() {
-		$(getHtmlForNewGoal("Test", 5.00)).insertBefore('.add-new-goal-col');
+		$(getHtmlForNewGoal("Test", '5.00')).insertBefore('.add-new-goal-col');
 	});
 
 	$('#openBtn').click(function(){
